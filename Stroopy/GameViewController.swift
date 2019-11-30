99fb55c9e6bac746632b896e51c000e7bd39c300
Jessica Trinh <jtjessicatrinh@gmail.com>
2019-11-29 17:55:20 -0800

    implement random textColor to UILabel

--- Stroopy/GameViewController.swift
+++ Stroopy/GameViewController.swift
@@ -13,7 +13,6 @@ import GameKit
 class GameViewController: UIViewController{
     
     let colorMeaning = ["black", "brown", "red", "orange", "yellow", "green", "blue", "purple", "pink"]
-//    let textColor = ["black", "brown", "red", "orange", "yellow", "green", "blue", "purple", "pink"]
     
     @IBOutlet weak var meaningLabel: UILabel!
     @IBOutlet weak var textColorLabel: UILabel!
@@ -37,16 +36,33 @@ class GameViewController: UIViewController{
     }
     
     
-    // helper function to change color meaning text
+    // helper function to change text in colorMeaningLabel
     func changeColorMeaning() {
         let randomIndex = Int.random(in: 0 ..< colorMeaning.count)
         meaningLabel.text = colorMeaning[randomIndex]
+        addColortoText()
     }
     
-        func changeTextColor() {
-            let randomIndex = Int.random(in: 0 ..< colorMeaning.count)
-            textColorLabel.text = colorMeaning[randomIndex]
-        }
-
+    // helper function to change text in textColorLabel
+    func changeTextColor() {
+        let randomIndex = Int.random(in: 0 ..< colorMeaning.count)
+        textColorLabel.text = colorMeaning[randomIndex]
+    }
+    
+    //helper function to change physical color of textColorLabel
+    func addColortoText() {
+        let black = #colorLiteral(red: 0, green: 0, blue: 0, alpha: 1)
+        let brown = #colorLiteral(red: 0.3098039329, green: 0.2039215714, blue: 0.03921568766, alpha: 1)
+        let red = #colorLiteral(red: 0.7450980544, green: 0.1568627506, blue: 0.07450980693, alpha: 1)
+        let orange = #colorLiteral(red: 0.9254902005, green: 0.2352941185, blue: 0.1019607857, alpha: 1)
+        let yellow = #colorLiteral(red: 0.9529411793, green: 0.6862745285, blue: 0.1333333403, alpha: 1)
+        let green = #colorLiteral(red: 0.2745098174, green: 0.4862745106, blue: 0.1411764771, alpha: 1)
+        let blue = #colorLiteral(red: 0.2196078449, green: 0.007843137719, blue: 0.8549019694, alpha: 1)
+        let purple = #colorLiteral(red: 0.09019608051, green: 0, blue: 0.3019607961, alpha: 1)
+        let pink = #colorLiteral(red: 0.8549019694, green: 0.250980407, blue: 0.4784313738, alpha: 1)
+        
+        self.meaningLabel.textColor = [black, brown, red, orange, yellow, green, blue, purple, pink].randomElement()
+        self.textColorLabel.textColor = [black, brown, red, orange, yellow, green, blue, purple, pink].randomElement()
+    }
     
 }
