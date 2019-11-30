bc63c426ac47cbc44fd23b9759a7147d19a0b02a
Jessica Trinh <jtjessicatrinh@gmail.com>
2019-11-29 21:35:59 -0800

    add game logic and score function

--- Stroopy/GameViewController.swift
+++ Stroopy/GameViewController.swift
@@ -18,6 +18,9 @@ class GameViewController: UIViewController{
     @IBOutlet weak var textColorLabel: UILabel!
     @IBOutlet weak var yesButton: UIButton!
     @IBOutlet weak var noButton: UIButton!
+    @IBOutlet weak var scoreLabel: UILabel!
+    var score = 0
+    @IBOutlet weak var timerLabel: UILabel!
     
     
 //    override func viewDidLoad() {
@@ -28,11 +31,13 @@ class GameViewController: UIViewController{
         print("yes was tapped")
         changeColorMeaning()
         changeTextColor()
+        updateScore()
     }
     @IBAction func noButtonTapped(_ sender: Any) {
         print("no was tapped")
         changeColorMeaning()
         changeTextColor()
+        updateScore()
     }
     
     
@@ -54,15 +59,27 @@ class GameViewController: UIViewController{
         let black = #colorLiteral(red: 0, green: 0, blue: 0, alpha: 1)
         let brown = #colorLiteral(red: 0.3098039329, green: 0.2039215714, blue: 0.03921568766, alpha: 1)
         let red = #colorLiteral(red: 0.7450980544, green: 0.1568627506, blue: 0.07450980693, alpha: 1)
-        let orange = #colorLiteral(red: 0.9254902005, green: 0.2352941185, blue: 0.1019607857, alpha: 1)
+        let orange = #colorLiteral(red: 0.9372549057, green: 0.3490196168, blue: 0.1921568662, alpha: 1)
         let yellow = #colorLiteral(red: 0.9529411793, green: 0.6862745285, blue: 0.1333333403, alpha: 1)
         let green = #colorLiteral(red: 0.2745098174, green: 0.4862745106, blue: 0.1411764771, alpha: 1)
-        let blue = #colorLiteral(red: 0.2196078449, green: 0.007843137719, blue: 0.8549019694, alpha: 1)
-        let purple = #colorLiteral(red: 0.09019608051, green: 0, blue: 0.3019607961, alpha: 1)
+        let blue = #colorLiteral(red: 0.2588235438, green: 0.7568627596, blue: 0.9686274529, alpha: 1)
+        let purple = #colorLiteral(red: 0.5568627715, green: 0.3529411852, blue: 0.9686274529, alpha: 1)
         let pink = #colorLiteral(red: 0.8549019694, green: 0.250980407, blue: 0.4784313738, alpha: 1)
         
         self.meaningLabel.textColor = [black, brown, red, orange, yellow, green, blue, purple, pink].randomElement()
         self.textColorLabel.textColor = [black, brown, red, orange, yellow, green, blue, purple, pink].randomElement()
     }
+    
+    // update score label
+    func updateScore() {
+        if meaningLabel.textColor == textColorLabel.textColor{
+            print("yay -- match")
+            score += 1
+            scoreLabel.text = String(score)
+        } else {
+            print("boo -- no match")
+        }
+    }
+    
     
 }
